ebff9eb61f3db87dc5e0b41cc9fbb0d4f4896df9
Lenis Liendro <lenis99lg@gmail.com>
2026-05-19 12:50:33 -0300

    Ejercicio 1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Hello, World!");
+int a;
+int b;
+a = 10;
+b = a;
+Console.WriteLine("valor de a: "+a);
+Console.WriteLine("Valor de b: "+b);
+
+//Ejercicio 1
+Console.WriteLine("Ingrese un numero:");
+string num = Console.ReadLine();
+int i = 0;
+int resto = 0;
+bool verificarNum = int.TryParse(num, out i);
+if (verificarNum)
+{
+    if (i>0)
+    {
+        while (i/10 != 0)
+        {
+            resto = resto*10 + i%10;
+            i /= 10;
+        }
+        resto = resto*10 + i;
+        Console.WriteLine("Numero invertido es: "+resto);
+    } else {
+        Console.WriteLine("Es un numero negativo");
+    }
+} else {
+    Console.WriteLine("No es un numero");
+}
+
