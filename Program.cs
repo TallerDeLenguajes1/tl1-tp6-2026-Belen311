b3f7ce352b5131cd47d45fe75f437fde966b61ab
Lenis Liendro <lenis99lg@gmail.com>
2026-05-19 13:18:28 -0300

    CalculadoraV1

--- Program.cs
+++ Program.cs
@@ -30,3 +30,49 @@ if (verificarNum)
     Console.WriteLine("No es un numero");
 }
 
+//Calculadora V1
+
+Console.WriteLine("Desea realizar un calculo?");
+string resp = Console.ReadLine();
+int j = 0;
+bool resultado = int.TryParse(resp, out j);
+
+while (resultado && (j != 0))
+{
+    Console.WriteLine("Ingrese el primer numero");
+    string num1 = Console.ReadLine();
+    double aux1 = 0;
+    double.TryParse(num1, out aux1);
+
+    Console.WriteLine("Ingrese el segundo numero");
+    string num2 = Console.ReadLine();
+    double aux2 = 0;
+    double.TryParse(num2, out aux2);
+
+    Console.WriteLine("Que tipo de calculo (suma, resta, multip o division):");
+    string tipoCalculo = Console.ReadLine();
+
+    switch (tipoCalculo)
+    {
+        case "1":
+        double suma = aux1 + aux2;
+        Console.WriteLine("Resultado "+suma);
+        break;
+        case "2":
+        double resta = aux1 - aux2;
+        Console.WriteLine("Resultado "+resta);
+        break;
+        case "3":
+        double multi = aux1 * aux2;
+        Console.WriteLine("Resultado "+multi);
+        break;
+        default:
+        double div = aux1 / aux2;
+        Console.WriteLine("Resultado "+div);
+        break;
+    }
+    Console.WriteLine("Desea realizar otro calculo?");
+    resp = Console.ReadLine();
+    resultado = int.TryParse(resp, out j);
+}
+
