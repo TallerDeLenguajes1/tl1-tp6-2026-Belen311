75e8a29e664adb317babdb11d12895b4b247754d
Lenis Liendro <lenis99lg@gmail.com>
2026-05-19 13:56:19 -0300

    Primera parte de ejercicio 4

--- Program.cs
+++ Program.cs
@@ -76,52 +76,7 @@ while (resultado && (j != 0))
     resultado = int.TryParse(resp, out j);
 }
 
-//Calculadora V1
-
-Console.WriteLine("Desea realizar un calculo?");
-string resp = Console.ReadLine();
-int j = 0;
-bool resultado = int.TryParse(resp, out j);
-
-while (resultado && (j != 0))
-{
-    Console.WriteLine("Ingrese el primer numero");
-    string num1 = Console.ReadLine();
-    double aux1 = 0;
-    double.TryParse(num1, out aux1);
-
-    Console.WriteLine("Ingrese el segundo numero");
-    string num2 = Console.ReadLine();
-    double aux2 = 0;
-    double.TryParse(num2, out aux2);
-
-    Console.WriteLine("Que tipo de calculo (suma, resta, multip o division):");
-    string tipoCalculo = Console.ReadLine();
-
-    switch (tipoCalculo)
-    {
-        case "1":
-        double suma = aux1 + aux2;
-        Console.WriteLine("Resultado "+suma);
-        break;
-        case "2":
-        double resta = aux1 - aux2;
-        Console.WriteLine("Resultado "+resta);
-        break;
-        case "3":
-        double multi = aux1 * aux2;
-        Console.WriteLine("Resultado "+multi);
-        break;
-        default:
-        double div = aux1 / aux2;
-        Console.WriteLine("Resultado "+div);
-        break;
-    }
-    Console.WriteLine("Desea realizar otro calculo?");
-    resp = Console.ReadLine();
-    resultado = int.TryParse(resp, out j);
-}
-
+//Calculadora V2
 Console.WriteLine("Ingrese un numero");
 string num3 = Console.ReadLine();
 double aux3 = 0;
@@ -160,3 +115,21 @@ Console.WriteLine("Maximo: "+max);
 
 double min = Math.Min(comp1, comp2);
 Console.WriteLine("Minimo: "+min);
+
+//Ejercicio 4
+Console.WriteLine("Ingrese un texto:");
+string texto = Console.ReadLine();
+int longitud = texto.Length;
+Console.WriteLine($"Longitud del texto: {longitud}");
+
+Console.WriteLine("Ingrese otro texto:");
+string texto2 = Console.ReadLine();
+
+Console.WriteLine($"Textos concatenados: {texto}{texto2}");
+
+string subCadena = texto.Substring(6);
+
+Console.WriteLine($"Cadena extraida: {subCadena}");
+
+Console.WriteLine($"La suma de  ");
+
