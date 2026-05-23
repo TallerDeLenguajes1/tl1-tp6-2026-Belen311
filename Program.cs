339ddd12ec86d0eb9e050a1df88182b5a4bb17b8
Beluluuu <sofibelengerman@gmail.com>
2026-05-22 21:24:41 -0300

    Tp terminado

--- Program.cs
+++ Program.cs
@@ -37,6 +37,8 @@ string resp = Console.ReadLine();
 int j = 0;
 bool resultado = int.TryParse(resp, out j);
 
+double suma, resta, multi, div;
+
 while (resultado && (j != 0))
 {
     Console.WriteLine("Ingrese el primer numero");
@@ -52,23 +54,30 @@ while (resultado && (j != 0))
     Console.WriteLine("Que tipo de calculo (suma, resta, multip o division):");
     string tipoCalculo = Console.ReadLine();
 
+    string numero1 = aux1.ToString();
+    string numero2 = aux2.ToString();
+
     switch (tipoCalculo)
     {
         case "1":
-        double suma = aux1 + aux2;
+        suma = aux1 + aux2;
         Console.WriteLine("Resultado "+suma);
+        Console.WriteLine($"La suma de {numero1} y de {numero2} es igual a: {suma.ToString()}");
         break;
         case "2":
-        double resta = aux1 - aux2;
+        resta = aux1 - aux2;
         Console.WriteLine("Resultado "+resta);
+        Console.WriteLine($"La resta de {numero1} y de {numero2} es igual a: {resta.ToString()}");
         break;
         case "3":
-        double multi = aux1 * aux2;
+        multi = aux1 * aux2;
         Console.WriteLine("Resultado "+multi);
+        Console.WriteLine($"La multiplicacion de {numero1} y de {numero2} es igual a: {multi.ToString()}");
         break;
         default:
-        double div = aux1 / aux2;
+        div = aux1 / aux2;
         Console.WriteLine("Resultado "+div);
+        Console.WriteLine($"La division de {numero1} y de {numero2} es igual a: {div.ToString()}");
         break;
     }
     Console.WriteLine("Desea realizar otro calculo?");
@@ -127,9 +136,75 @@ string texto2 = Console.ReadLine();
 
 Console.WriteLine($"Textos concatenados: {texto}{texto2}");
 
-string subCadena = texto.Substring(6);
+string subCadena = texto.Substring(2);
 
 Console.WriteLine($"Cadena extraida: {subCadena}");
 
-Console.WriteLine($"La suma de  ");
+foreach (char item in texto)
+{
+    Console.WriteLine(item);
+}
 
+Console.WriteLine("Ingrese la palabra a buscar: ");
+string buscar = Console.ReadLine();
+
+int indice = texto.IndexOf(buscar);
+
+if (indice != -1)
+{
+    Console.WriteLine("Se encontro");
+}
+
+string mayus = texto.ToUpper();
+string minus = texto.ToLower();
+
+Console.WriteLine(mayus);
+Console.WriteLine(minus);
+
+string []textoDiv = texto.Split("@");
+
+foreach (string palabra in textoDiv)
+{
+    Console.WriteLine(palabra);
+}
+
+Console.WriteLine("Ingrese una operacion: ");
+string operacion = Console.ReadLine();
+
+int var1, var2;
+
+
+foreach (char signo in operacion)
+{
+    switch (signo)
+    {
+        case '+':
+        string []op = operacion.Split('+');
+        var1 = int.Parse(op[0]);
+        var2 = int.Parse(op[1]);
+        suma = var1 + var2;
+        Console.WriteLine(suma);
+        break;
+        case '-':
+        string []ope = operacion.Split('-');
+        var1 = int.Parse(ope[0]);
+        var2 = int.Parse(ope[1]);
+        resta = var1 + var2;
+        Console.WriteLine(resta);
+        break;
+        case '*':
+        string []oper = operacion.Split('*');
+        var1 = int.Parse(oper[0]);
+        var2 = int.Parse(oper[1]);
+        multi = var1 * var2;
+        Console.WriteLine(multi);
+        break;
+        case '/':
+        string []opera = operacion.Split('/');
+        var1 = int.Parse(opera[0]);
+        var2 = int.Parse(opera[1]);
+        div = var1 / var2;
+        Console.WriteLine(div);
+        break;
+    }
+}
